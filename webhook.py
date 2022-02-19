77135b5caa6943732c960faf77479bd31b4fe99e
Horley1 <41126653+Horley1@users.noreply.github.com>
2022-02-19 19:40:28 +0300

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -3,6 +3,7 @@ import psycopg2
 from flask import Flask, request
 from config import *
 import telebot
+from random import randint
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
@@ -20,10 +21,26 @@ def help(message):
     bot.send_message(message.chat.id, "Напиши: /start")
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['reg'])
+def reg(message):
+    if not check_bd(message):
+        bot.send_message(message.chat.id, "Напиши мне свой логин:")
+        #bot.register_next_step_handler(message, get_login)
+    else:
+        bot.send_message(message.chat.id, "Эййй, ты уже в базе!")
 
+def check_bd(message):
+    if cursor.execute(f"SELECT * FROM data WHERE user_id={message.chat.id}").fetchone():
+        return True
+    else:
+        return False
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def help(message):
+    res = bot.send_message(message.chat.id, phrases[randint(0, 7)])
+    bot.send_sticker(message.chat.id, ids[randint(0, 14)], res.id)
+    bot.send_message(message.chat.id, str(message.chat.id))
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
