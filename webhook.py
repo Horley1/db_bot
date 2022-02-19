4f2dc7cc774224e169912f82f277364c222a7488
Horley1 <41126653+Horley1@users.noreply.github.com>
2022-02-19 19:36:04 +0300

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -1,17 +1,23 @@
 import os
-
+import psycopg2
 from flask import Flask, request
-
+from config import *
 import telebot
 
-TOKEN = '5199364372:AAGnaM9JbpyH2_JjTpCi1zb3EN5nWUtiwmE'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
-
+conn = psycopg2.connect(dbname='d23v4g77tn2j92', user='qzusajqercdmfq',
+                        password='36da4de8c545b260b07dccc490b56cee3fcc72ee52a073e7fb40409e8ccf47c4', host='ec2-52-31-217-108.eu-west-1.compute.amazonaws.com')
+cursor = conn.cursor()
+conn.autocommit = True
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет! Если ты ещё не зарегался пиши: "/reg".')
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "Напиши: /start")
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
@@ -27,14 +33,7 @@ def getMessage():
     return "!", 200
 
 
-# @server.route("/")
-# def webhook():
-#     bot.remove_webhook()
-#     bot.set_webhook(url='https://eltg.herokuapp.com/' + TOKEN)
-#     return "!", 200
-
-
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url='https://elgurbot.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url=URL)
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
