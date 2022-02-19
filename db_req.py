741bfd4e21b751aed2aeda3dac24061b1e561453
Horley1 <41126653+Horley1@users.noreply.github.com>
2022-02-19 22:10:29 +0300

    Add files via upload

--- db_req.py
+++ db_req.py
@@ -7,27 +7,35 @@ import time
 from datetime import datetime
 from fernet import *
 import psycopg2
+from flask import Flask, request
+import os
 
 bot = telebot.TeleBot(TOKEN)
 conn = psycopg2.connect(dbname='d23v4g77tn2j92', user='qzusajqercdmfq',
-                        password='36da4de8c545b260b07dccc490b56cee3fcc72ee52a073e7fb40409e8ccf47c4', host='ec2-52-31-217-108.eu-west-1.compute.amazonaws.com')
+                        password='36da4de8c545b260b07dccc490b56cee3fcc72ee52a073e7fb40409e8ccf47c4',
+                        host='ec2-52-31-217-108.eu-west-1.compute.amazonaws.com')
 cursor = conn.cursor()
 conn.autocommit = True
+server = Flask(__name__)
+
 
 def parsing_process(message_id):
     cursor.execute(f"SELECT * FROM data WHERE user_id={message_id}")
     txt = cursor.fetchone()
     new_txt = get_elgur_by_token(txt[3], message_id)
     txt = json.loads(txt[4])
-    if  new_txt != txt:
+    if new_txt != txt:
         for i in range(16):
             if txt[i] != new_txt[i]:
                 ln1 = len(txt[i]['marks'])
                 ln2 = len(new_txt[i]['marks'])
                 for j in range(ln2):
                     if j > len(txt[i]['marks']) - 1 or new_txt[i]['marks'][j] != txt[i]['marks'][j]:
-                        bot.send_message(message_id, f"У тебя новая оценка по {new_txt[i]['name']}\nОценка: <tg-spoiler>{new_txt[i]['marks'][j]['value']}</tg-spoiler> ✅\nТип: {new_txt[i]['marks'][j]['lesson_comment']}\nДата: {new_txt[i]['marks'][j]['date']}", parse_mode="HTML")
+                        bot.send_message(message_id,
+                                         f"У тебя новая оценка по {new_txt[i]['name']}\nОценка: <tg-spoiler>{new_txt[i]['marks'][j]['value']}</tg-spoiler> ✅\nТип: {new_txt[i]['marks'][j]['lesson_comment']}\nДата: {new_txt[i]['marks'][j]['date']}",
+                                         parse_mode="HTML")
         add_to_bd(message_id, new_txt)
+
 
 def check_date(message_id):
     cursor.execute(f"SELECT * FROM data WHERE user_id={message_id}")
@@ -48,6 +56,7 @@ def decode(data):
     decrypted_text = cipher.decrypt(str.encode(data, encoding='utf-8'))
     return str(decrypted_text)[2:-1]
 
+
 def change_token(message_id):
     login = cursor.execute(f"SELECT * FROM data WHERE user_id={message_id}").fetchone()[1]
     password = cursor.execute(f"SELECT * FROM data WHERE user_id={message_id}").fetchone()[2]
@@ -62,7 +71,6 @@ def change_token(message_id):
     value = str("'") + token + str("'")
     cursor.execute(f"UPDATE data SET token = {value} WHERE user_id = {message_id}")
     return token
-
 
 
 def get_elgur_by_token(token, message_id):
@@ -80,18 +88,20 @@ def get_elgur_by_token(token, message_id):
     lst_marks = r2.json()['response']['result']['students'][student_code]['lessons']
     return lst_marks
 
+
 def add_to_bd(message_id, new_list):
-    values = [message_id ,str("'") + json.dumps(new_list) + str("'")]
+    values = [message_id, str("'") + json.dumps(new_list) + str("'")]
     cursor.execute(f"UPDATE data SET last_marks = {values[1]} WHERE user_id = {values[0]}")
 
 
-cursor.execute("SELECT user_id FROM data")
-test = cursor.fetchall()
-while True:
+@server.route('/')
+def req():
+    cursor.execute("SELECT user_id FROM data")
+    test = cursor.fetchall()
     for elem in test:
         try:
             parsing_process(elem[0])
         except:
             pass
-    time.sleep(0.1)
-    print("YES!!!")
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
