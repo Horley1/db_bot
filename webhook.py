6baf7e6ad864e01be8aedb44ca6ebab4e820eb39
Horley1 <41126653+Horley1@users.noreply.github.com>
2022-02-19 16:15:54 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -27,12 +27,14 @@ def getMessage():
     return "!", 200
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://eltg.herokuapp.com/' + TOKEN)
-    return "!", 200
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://eltg.herokuapp.com/' + TOKEN)
+#     return "!", 200
 
 
 if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url='https://eltg.herokuapp.com/')
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
