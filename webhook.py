a190e1bcd7df9104f7fd0a3a4d0e622885c0af57
Horley1 <41126653+Horley1@users.noreply.github.com>
2022-03-31 09:07:48 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -377,23 +377,20 @@ def process_callback_button23(callback_query):
     bot.edit_message_reply_markup(callback_query.from_user.id, callback_query.message.message_id, reply_markup=keyboard)
     cursor.execute(f"UPDATE data SET marks = false WHERE user_id = {callback_query.from_user.id}")
 
-# @server.route('/' + TOKEN, methods=['POST'])
-# def getMessage():
-#     json_string = request.get_data().decode('utf-8')
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])
-#     return "!", 200
-#
-# @server.route('/')
-# def startPage():
-#     resp = jsonify(success=True)
-#     resp.status_code = 200
-#     return resp
-#
-# if __name__ == "__main__":
-#     bot.remove_webhook()
-#     bot.set_webhook(url=URL)
-#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
-bot.remove_webhook()
-bot.polling()
+@server.route('/')
+def startPage():
+    resp = jsonify(success=True)
+    resp.status_code = 200
+    return resp
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
